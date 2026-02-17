3be1ea5c1f744028ce4e5219081189574edbadd3
MD ABDUR RAHMAN SHOAIB <mdabdurrahmanshoaib@gmail.com>
2026-02-18 05:02:28 +0600

    Design Cashout Form and logical implementation

---
+++ script/cashout.js
@@ -0,0 +1,9 @@
+document.getElementById("cashout-btn").addEventListener('click',  function(){
+  // get the agent num && validate
+  //get the amount , validate, convert to number
+  //get the current balance , validate, convert to number
+  // get calculate new balance 
+  // get the pin && verify
+  // isTrue :: show an alert > set balance 
+  // isFalse :: show an error alert > return
+})
